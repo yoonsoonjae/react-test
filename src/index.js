0466b015ac9b0c0c2325eb3dda802d87eaf43347
yoonsoonjae <asd67200@gmail.com>
2024-11-17 18:12:54 +0900

    추가

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; // BrowserRouter 임포트
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}> {/* BrowserRouter로 감싸기 */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// 성능 측정을 위한 reportWebVitals
+reportWebVitals()
